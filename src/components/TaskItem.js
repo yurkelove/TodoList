81267bacb3718d5c8077a62694762bb806467cb9
Yura Kuzkin <yurkelove28@gmail.com>
2018-11-09 14:25:46 +0200

    fix unicode

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -12,11 +12,13 @@ class TaskItem extends PureComponent{
     }
 
     render(){
+        const unDone = String.fromCharCode(10006);
+        const unFalse = String.fromCharCode(10004);
         return(
         <li className={`todo__item`}>
         <span className={this.props.item.isDone ? 'item__done' : ''}>{this.props.item.text}</span>
         <button className={`button__item`} >
-            {this.props.item.isDone ? String.fromCharCode(10006) : String.fromCharCode(10004)}
+            {this.props.item.isDone ? unDone : unFalse }
         </button>
         </li>
         )
