86f3c7be38f73cc956af08435a70ad981cff4379
Yura Kuzkin <yurkelove28@gmail.com>
2018-11-12 11:07:03 +0200

    fix props

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,27 +1,25 @@
 import React ,{PureComponent}  from  "react";
-
+const cross = String.fromCharCode(10006);
+const mark = String.fromCharCode(10004);
 
 
 class TaskItem extends PureComponent{
 
     render(){
-        const {isDone_X} = this.props;
-        const cross = String.fromCharCode(10006);
-        const mark = String.fromCharCode(10004);
+        const {toogleTask} = this.props;
         return(
         <li className={`todo__item`}>
-             <span className={isDone_X ? 'item__done' : ''}>
+             <span className={toogleTask ? 'item__done' : ''}>
                  {this.props.item.text}
              </span>
-             <button className={`button__item`} >
-                {this.isDone_X ? cross : mark }
+             <button className={`button__item`} onClick={this.handleClick}>
+                {toogleTask ? cross : mark }
              </button>
         </li>
         )
     }
 
     handleClick = () =>{
-
         this.setState({
             isDone : !this.state.isDone
      })
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,7 +6,7 @@ import TaskItem from "./TaskItem";
 
 const TaskList = (props) => {
     const listElements = props.items.map((item) =>
-        <TaskItem key={item.id} item = {item}/>
+        <TaskItem key={item.id} item = {item} toogleTask = {props.toogleTask}/>
     );
     return(
         <div className ="TodoList__listArea">
